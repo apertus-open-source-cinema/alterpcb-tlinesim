db6192f91a1ec3b56941540a8d54040536ce3ccb
Andrej <b.andrej@cumir.de>
2019-04-02 22:39:13 +0200

    - Fixed conversion bug

--- src/gui/CustomLineEdit.cpp
+++ src/gui/CustomLineEdit.cpp
@@ -4,64 +4,53 @@
 #include <QLineEdit>
 #include <QPushButton>
 
-CustomLineEdit::CustomLineEdit(bool enableUnitButton, QWidget *parent) : QWidget(parent)
-{
-    QHBoxLayout *layout = new QHBoxLayout( this );
-    layout->setMargin( 0 );
+CustomLineEdit::CustomLineEdit(bool enableUnitButton, QWidget *parent)
+    : QWidget(parent) {
+  QHBoxLayout *layout = new QHBoxLayout(this);
+  layout->setMargin(0);
 
-    lineEdit = new QLineEdit("filechooser_lineedit", this);
-    layout->addWidget( lineEdit );
+  lineEdit = new QLineEdit("filechooser_lineedit", this);
+  layout->addWidget(lineEdit);
 
-    if(!enableUnitButton)
-    {
-        return;
-    }
+  if (!enableUnitButton) {
+    return;
+  }
 
-    button = new QPushButton( "mm", this);
-    button->setFixedWidth(button->fontMetrics().width( "  mm  " ) );
-    button->setCheckable(true);
-    connect( button, SIGNAL(toggled(bool)), this, SLOT( UpdateUnitText(bool)));
-    layout->addWidget( button );
+  button = new QPushButton("mm", this);
+  button->setFixedWidth(button->fontMetrics().width("  mm  "));
+  button->setCheckable(true);
+  connect(button, SIGNAL(toggled(bool)), this, SLOT(UpdateUnitText(bool)));
+  layout->addWidget(button);
 }
 
-void CustomLineEdit::SetText(QString text)
-{
-    originalText = text;
-    lineEdit->setText(text);
+void CustomLineEdit::SetText(QString text) {
+  originalText = text;
+  lineEdit->setText(text);
 }
 
-QString CustomLineEdit::GetText()
-{
-    return GetMMValue();
+QString CustomLineEdit::GetText() { return GetMMValue(); }
+
+void CustomLineEdit::UpdateUnitText(bool checked) {
+  double value = lineEdit->text().toDouble();
+
+  if (checked) {
+    button->setText("mil");
+
+    lineEdit->setText(QString::number(value * 39.37008));
+  } else {
+    button->setText("mm");
+
+    lineEdit->setText(QString::number(value * 0.0254));
+  }
 }
 
-void CustomLineEdit::UpdateUnitText(bool checked)
-{
-    double value = lineEdit->text().toDouble();
+QString CustomLineEdit::GetMMValue() {
+  QString result = lineEdit->text();
 
-    if(checked)
-    {
-        button->setText("mil");
+  if (button->isChecked()) {
+    double value = result.toDouble();
+    result = QString::number(value * 0.0254);
+  }
 
-        lineEdit->setText(QString::number(value * 39.37008));
-    }
-    else
-    {
-        button->setText("mm");
-
-        lineEdit->setText(QString::number(value * 0.0254));
-    }
-}
-
-QString CustomLineEdit::GetMMValue()
-{
-    QString result = lineEdit->text();
-
-    if(button->isChecked())
-    {
-        double value = result.toDouble();
-        result = QString::number(value * 39.37008);
-    }
-
-    return result;
+  return result;
 }
